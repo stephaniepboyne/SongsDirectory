8582523b57d467e1fd51f97071c39d099d76504e
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-08-03 13:28:48 +0100

    models done

--- src/main/java/com/codeclan/example/elvis/models/Song.java
+++ src/main/java/com/codeclan/example/elvis/models/Song.java
@@ -39,8 +39,81 @@ public class Song{
     @JoinColumn(name="album_id", nullable=false)
     private Album album;
 
-    public Song {
+    public Song(String title, boolean listened, int personalRating, String albumCoverURL, String lyrics, Songwriter songwriter, Album album) {
+        this.title = title;
+        this.listened = listened;
+        this.personalRating = personalRating;
+        this.albumCoverURL = albumCoverURL;
+        this.lyrics = lyrics;
+        this.songwriter = songwriter;
+        this.album = album;
+    }
 
+    public Song() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isListened() {
+        return listened;
+    }
+
+    public void setListened(boolean listened) {
+        this.listened = listened;
+    }
+
+    public int getPersonalRating() {
+        return personalRating;
+    }
+
+    public void setPersonalRating(int personalRating) {
+        this.personalRating = personalRating;
+    }
+
+    public String getAlbumCoverURL() {
+        return albumCoverURL;
+    }
+
+    public void setAlbumCoverURL(String albumCoverURL) {
+        this.albumCoverURL = albumCoverURL;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    public Songwriter getSongwriter() {
+        return songwriter;
+    }
+
+    public void setSongwriter(Songwriter songwriter) {
+        this.songwriter = songwriter;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
     }
 }
 
--- src/main/java/com/codeclan/example/elvis/models/Songwriter.java
+++ src/main/java/com/codeclan/example/elvis/models/Songwriter.java
@@ -1,4 +1,54 @@
 package com.codeclan.example.elvis.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="songwriters")
 public class Songwriter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @JsonIgnoreProperties({"songwriter"})
+    @OneToMany(mappedBy = "songwriter")
+    private List<Song> songs;
+
+    public Songwriter(String name, List<Song> songs) {
+        this.name = name;
+        this.songs = songs;
+    }
+
+    public Songwriter(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
 }
